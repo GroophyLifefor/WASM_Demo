4bb0ab70ef84b1043023e7c6fc492890d5f95a00
Groophylifefor <groophylifefor@gmail.com>
2024-02-17 20:09:35 +0300

    initalize

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace WASM_Demo;
+
+public partial class Program
+{
+    static void Main(string[] args) { }
+
+    [JSExport]
+    public static string Response()
+    {
+        return """
+               <h1>
+                   Hello World
+               </h1>
+               """;
+    }
+}
---
+++ main.js
@@ -0,0 +1,23 @@
+﻿import { dotnet } from './dotnet.js'
+
+const is_browser = typeof window != "undefined";
+if (!is_browser) throw new Error(`Expected to be running in a browser`);
+
+const { setModuleImports, getAssemblyExports, getConfig, runMainAndExit } = await dotnet
+    .withDiagnosticTracing(false)
+    .withApplicationArgumentsFromQuery()
+    .create();
+
+const config = getConfig();
+const exports = await getAssemblyExports(config.mainAssemblyName);
+
+const html = 
+    exports
+        .WASM_Demo    // Namespace
+        .Program      // Class Name
+        .Response();  // Function Name
+
+// Regular javascript code
+document.getElementById("app").innerHTML = `${html}`;
+
+await runMainAndExit(config.mainAssemblyName, [] /* Console App Args */);
